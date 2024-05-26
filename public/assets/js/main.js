c64a5b06aac2e16639d16d07e96659423efa5a2d
Sam Stephens <scstephe@uci.edu>
2024-05-26 12:23:19 -0700

    part 1 final lobby

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,14 +9,18 @@ function getIRIParameterValue(requestedKey) {
             return value;
         }
     }
+    return null;
 }
 
 let username = decodeURI(getIRIParameterValue('username'));
-if((typeof username == 'undefined') || (username === null)) {
+if((typeof username == 'undefined') || (username === null) || (username === 'null')) {
     username = "Anonymous_"+Math.floor(Math.random()*1000);
 }
 
-let chatRoom = 'Lobby';
+let chatRoom = decodeURI(getIRIParameterValue('game_id'));
+if((typeof chatRoom == 'undefined') || (chatRoom === null) || (chatRoom === 'null')) {
+    chatRoom = "Lobby";
+}
 
 /* Set up the socket.io connection to the server */
 let socket = io();
@@ -33,8 +37,11 @@ socket.on('join_room_response', (payload) => {
         console.log(payload.message);
         return;
     }
-    let newString= '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
-    $('#messages').prepend(newString);
+    let newHTML= '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade",500);
 });
 
 function sendChatMessage() {
@@ -44,6 +51,7 @@ function sendChatMessage() {
     request.message = $('#chatMessage').val();
     console.log('**** Client log message, sending \'send_chat_message\' command: '+JSON.stringify(request));
     socket.emit('send_chat_message',request);
+    $('#chatMessage').val("");
 }
 
 socket.on('send_chat_message_response', (payload) => {
@@ -55,8 +63,11 @@ socket.on('send_chat_message_response', (payload) => {
         console.log(payload.message);
         return;
     }
-    let newString= '<p class=\'chat_message\'><b>'+payload.username+'</b>: '+payload.message+'</p>';
-    $('#messages').prepend(newString);
+    let newHTML = '<p class=\'chat_message\'><b>'+payload.username+'</b>: '+payload.message+'</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade",500);
 });
 
 /* Request to join the chat room */
@@ -66,5 +77,15 @@ $( () => {
     request.username = username;
     console.log('**** Client log message, sending \'join_room\' command: '+JSON.stringify(request));
     socket.emit('join_room',request);
+
+    $("#lobbyTitle").html(username + "'s Lobby");
+
+    $('#chatMessage').keypress(function (e) {
+        let key = e.which;
+        if(key == 13) { // the enter key
+            $('button[id = chatButton]').click();
+            return false;
+        }
+      })
 
 });
