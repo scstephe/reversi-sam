d7a1a18859656bb88aecf6d409e170410ebf4036
Sam Stephens <scstephe@uci.edu>
2024-05-29 08:24:10 -0700

    Unfinished Red Error Board code

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ io.on('connection', (socket) => {
                     username: username,
                     room: room
                 }
-                /* Announcance to everyone who in the room who else is in the room*/
+                /* Announce to everyone who in the room who else is in the room*/
                 for(const member of sockets) {
                     response = {
                         result: 'success',
@@ -143,6 +143,9 @@ io.on('connection', (socket) => {
                     /*Tell everyone that a new user has joined the chat room*/
                     io.of('/').to(room).emit('join_room_response',response);
                     serverLog('join_room succeeded ', JSON.stringify(response));
+                    if(room !== "Lobby") {
+                        send_game_update(socket,room, 'initial update');
+                    }
                 }
             }
         });
@@ -458,6 +461,68 @@ io.on('connection', (socket) => {
     });
 
 
+/******************************/
+/* Code related to game state */
+
+let games = [];
+
+function create_new_game() {
+    let new_game = {};
+    new_game.player_white = {};
+    new_game.player_white.socket = "";
+    new_game.player_white.username = "";
+    new_game.player_black = {};
+    new_game.player_black.socket = "";
+    new_game.player_black.username = "";
+
+    var d = new Date();
+    new_game.last_move_time = d.getTime();
+
+    new_game.whose_turn = 'white';
+
+    new_game.board = [
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ','w','b',' ',' ',' '],
+        [' ',' ',' ','b','w',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' ']
+
+    ];
+    return new_game;
+
+}
+
+function send_game_update(socket, game_id, message) {
+
+    /*Check to see if a game with game id_exists*/
+    /*Make sure that only two people are in a room*/
+    /*Assign this socket a color*/
+    /*Send game update*/
+    /*Check if the game is over*/
+
+    /*Check to see if a game with game id_exists*/
+    if ((typeof games[game_id] == 'undefined') || (games[game_id]===null)) {
+        console.log("No game exists with game_id:" + game_id + ". Making a new game for " + socket.id);
+        games[game_id] = create_new_game();
+    }
+
+
+    /*Send game update*/
+    let payload = {
+        result: 'success',
+        game_id: game_id,
+        game: games[game_id],
+        message: message
+    }
+
+    io.of("/").to(game_id).emit('game_update', payload);
+
+
+
+}
 
 
 
