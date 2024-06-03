ff6fedb0c2bd63c5dcefded417547a242a025cf1
Sam Stephens <scstephe@uci.edu>
2024-06-02 18:41:47 -0700

    FINAL PROJECT DONNEEEEE

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -242,17 +242,18 @@ socket.on('send_chat_message_response', (payload) => {
 });
 
 let old_board = [
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?']
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ']
 ];
 
 let my_color = "";
+let interval_timer;
 
 socket.on('game_update', (payload) => {
     if(( typeof payload == 'undefined') || (payload === null)){
@@ -393,6 +394,33 @@ socket.on('game_update', (payload) => {
                 }           
             }
     }
+
+    clearInterval(interval_timer)
+    interval_timer = setInterval(((last_time) => {
+        return(() => {
+            let d = new Date();
+            let elapsed_m = d.getTime() - last_time;
+            let minutes = Math.floor((elapsed_m/1000)/60);
+            let seconds = Math.floor((elapsed_m %(60 * 1000))/1000); //typo???
+            let total = minutes * 60 + seconds;
+            if(total >100) {
+                total=100;
+            }
+            $("#elapsed").css("width",total+"%").attr("aria-valuenow",total);
+            let timestring = ""+seconds;
+            timestring = timestring.padStart(2, '0');
+            timestring = minutes+":"+timestring;
+            if(total < 100){
+                $("#elapsed").html(timestring);
+            }
+            else {
+                $("#elapsed").html("Times up!");
+            }
+            
+        })
+
+    })(payload.game.last_move_time),1000);
+
     $("#whitesum").html(whitesum);
     $("#blacksum").html(blacksum);
     old_board = board;
--- server.js
+++ server.js
@@ -583,6 +583,9 @@ io.on('connection', (socket) => {
                 game.legal_moves = calculate_legal_moves('w',game.board);
             }
 
+            let d = new Date();
+            game.last_move_time = d.getTime();
+
             send_game_update(socket, game_id, 'played a token');
          
         }); 
